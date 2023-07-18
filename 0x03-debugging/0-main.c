7059ddee5a61336c0abacaa8a508202ad7b89c4d
ezehchika <ezehchika0@gmail.com>
2023-07-18 16:59:58 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -20,19 +20,3 @@ int main(void)
     positive_or_negative(i);
     return (0);
 }
-
-/**
- * positive_or_negative - Function to determine if a number is positive or negative
- * @i: The number to check
- *
- * Return: No return value (void)
- */
-void positive_or_negative(int i)
-{
-    if (i > 0)
-        printf("%d is positive\n", i);
-    else if (i < 0)
-        printf("%d is negative\n", i);
-    else
-        printf("%d is zero\n", i);
-}
